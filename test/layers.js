d25ea9b9892eb3f1a5c2f246941976c1154112ec
ajlopez <webmaster@ajlopez.com>
2018-10-13 14:28:07 -0300

    Get layer neurons

--- test/layers.js
+++ test/layers.js
@@ -11,3 +11,17 @@ exports['create layer with one dimension'] = function (test) {
     test.deepEqual(layer.shape(), [ 4 ]);
 };
 
+exports['get layer neurons having relu'] = function (test) {
+    var layer = sn.layer(4);
+    
+    for (var k = 0; k < 4; k++) {
+        var neuron = layer.neuron(k);
+        
+        test.ok(neuron);
+        test.equal(neuron.output(-1), 0);
+        test.equal(neuron.output(0), 0);
+        test.equal(neuron.output(1), 1);
+        test.equal(neuron.output(2), 2);
+    }
+};
+

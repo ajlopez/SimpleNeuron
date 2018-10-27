3fb7dd1e457a12a73d6f71142ad9004c4aa86104
ajlopez <webmaster@ajlopez.com>
2018-10-27 13:04:26 -0300

    Removing neuron.input, using neuron.inputs/weights

--- test/neuron.js
+++ test/neuron.js
@@ -39,7 +39,8 @@ exports['set input and get output'] = function (test) {
     var neuron = sn.neuron();
     var input = sn.neuron();
     
-    neuron.input(input);
+    neuron.inputs([input]);
+    neuron.weights([1]);
     input.value(1);
     
     test.equal(neuron.output(), 1);
@@ -50,8 +51,8 @@ exports['two inputs'] = function (test) {
     var input2 = sn.neuron();
     var neuron = sn.neuron();
     
-    neuron.input(input1);
-    neuron.input(input2);
+    neuron.inputs([input1, input2]);
+    neuron.weights([1, 1]);
     input1.value(0.5);
     input2.value(0.5);
     test.equal(neuron.output(), 1);
@@ -76,8 +77,8 @@ exports['two inputs with bias'] = function (test) {
     var input2 = sn.neuron();
     var neuron = sn.neuron({ bias: 2 });
     
-    neuron.input(input1);
-    neuron.input(input2);
+    neuron.inputs([input1, input2]);
+    neuron.weights([1, 1]);
     input1.value(0.5);
     input2.value(0.5);
     test.equal(neuron.output(), 3);
@@ -97,8 +98,9 @@ exports['two inputs with weights'] = function (test) {
     var input2 = sn.neuron();
     var neuron = sn.neuron();
     
-    neuron.input(input1, 0.5);
-    neuron.input(input2, -0.5);
+    neuron.inputs([input1, input2]);
+    neuron.weights([ 0.5, -0.5]);
+
     input1.value(1);
     input2.value(1);
     test.equal(neuron.output(), 0);
@@ -108,7 +110,8 @@ exports['create neuron with default function'] = function (test) {
     var neuron = sn.neuron();
     var input = sn.neuron({ fn: 'direct' });
     
-    neuron.input(input);
+    neuron.inputs([input]);
+    neuron.weights([1]);
     input.value(1);
     
     test.equal(input.value(), 1);
@@ -127,7 +130,8 @@ exports['create neuron with direct function'] = function (test) {
     var neuron = sn.neuron({ fn: 'direct' });
     var input = sn.neuron({ fn: 'direct' });
     
-    neuron.input(input);
+    neuron.inputs([input]);
+    neuron.weights([1]);
     input.value(1);
     
     test.equal(neuron.value(), 1);
@@ -144,7 +148,8 @@ exports['create neuron with logistic function'] = function (test) {
     var neuron = sn.neuron({ fn: 'logistic' });
     var input = sn.neuron({ fn: 'direct' });
     
-    neuron.input(input);
+    neuron.inputs([input]);
+    neuron.weights([1]);
     input.value(0);
     
     test.equal(input.value(), 0);
